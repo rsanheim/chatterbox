08af2b696416b4dce33710311a856c708382b99a
Rob Sanheim <rsanheim@gmail.com>
2009-11-07 22:44:26 -0500

    Clean up and exceptions to ignore

--- examples/lib/chatterbox/rails_catcher_example.rb
+++ examples/lib/chatterbox/rails_catcher_example.rb
@@ -2,60 +2,18 @@ require 'example_helper'
 require 'chatterbox/exception_notification'
 require File.expand_path(File.join(File.dirname(__FILE__), *%w[.. .. .. rails init]))
 
-ActionController::Routing::Routes.draw { |map| map.connect ':controller/:action/:id' }
-
-class WidgetException < RuntimeError; end
-class WidgetsController < ActionController::Base
-  include Chatterbox::RailsCatcher
-
-  def rescue_action e
-    rescue_action_in_public e
+describe Chatterbox::RailsCatcher do
+  
+  def helper
+    @helper ||= Class.new {
+      include Chatterbox::RailsCatcher
+    }.new
   end
   
-  def rescue_action_in_public_without_chatterbox e
-    raise e
-  end
-  
-  def index
-    raise_exception
-    render :text => "hi"
-  end
-  
-  def raise_exception
-    raise WidgetException, "Bad dog!"
-  end
-end
-
-describe WidgetsController do
-  
-  describe "controller haxing" do
-    it "chains the chatterbox method to rescue_action_in_public" do
-      exception = RuntimeError.new
-      @controller.expects(:rescue_action_in_public_without_chatterbox).with(exception)
-      @controller.stubs(:extract_exception_details)
-      @controller.rescue_action_in_public(exception)
-    end
-  
-    it "should have the catcher included in ApplicationController" do
-      WidgetsController.ancestors.should include(Chatterbox::RailsCatcher)
-    end
-    
-    it "should hide aliased methods so they are not exposed as actions" do
-      WidgetsController.hidden_actions.should include("rescue_action_in_public_with_chatterbox")
-      WidgetsController.hidden_actions.should include("rescue_action_in_public_without_chatterbox")
-    end
-  end
-  
-  describe "exception handling" do
-    it "should raise on index" do
-      lambda {
-        get :index
-      }.should raise_error(WidgetException, "Bad dog!")
-    end
-    
-    it "should send exception notice as hash" do
-      Chatterbox.expects(:handle_notice).with(instance_of(Hash))
-      get :index rescue nil
+  describe "logger" do
+    it "should delegate to Chatterbox#logger" do
+      Chatterbox.expects(:logger)
+      helper.logger
     end
   end
 end
--- lib/chatterbox/rails_catcher.rb
+++ lib/chatterbox/rails_catcher.rb
@@ -1,6 +1,17 @@
+require 'ostruct'
+
 module Chatterbox
 
   module RailsCatcher
+    delegate :logger, :to => Chatterbox
+    
+    def self.configuration
+      @configuration ||= OpenStruct.new(:ignore => [])
+    end
+    
+    def self.configure
+      yield(configuration)
+    end
     
     def self.included(base)
       if base.instance_methods.map(&:to_s).include? 'rescue_action_in_public' and !base.instance_methods.map(&:to_s).include? 'rescue_action_in_public_without_chatterbox'
@@ -12,13 +23,21 @@ module Chatterbox
     
     # Overrides the rescue_action method in ActionController::Base, but does not inhibit
     # any custom processing that is defined with Rails 2's exception helpers.
-    def rescue_action_in_public_with_chatterbox exception
-      Chatterbox.logger.debug { "#{self.class}#rescue_action_in_public_with_chatterbox: caught exception #{exception} - about to handle"}
-      options = extract_exception_details(exception)
-      Chatterbox.handle_notice(options)
-      Chatterbox.logger.debug { "#{self.class}#rescue_action_in_public_with_chatterbox: handing exception #{exception} off to normal rescue handling"}
-      
+    def rescue_action_in_public_with_chatterbox(exception)
+      logger.debug { "#{log_prefix} caught exception #{exception} - about to handle" }
+      unless on_ignore_list?(exception)
+        Chatterbox.handle_notice(extract_exception_details(exception))
+      end
+      logger.debug { "#{log_prefix} handing exception #{exception} off to normal rescue handling" }
       rescue_action_in_public_without_chatterbox(exception)
+    end
+    
+    def log_prefix
+      "#{self.class}#rescue_action_in_public_with_chatterbox:"
+    end
+    
+    def on_ignore_list?(exception)
+      RailsCatcher.configuration.ignore.include?(exception.class)
     end
     
     def extract_exception_details(exception)
