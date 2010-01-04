b43d7a9ea185f5d6aca74ab8e6a0d4b09d8137bb
Rob Sanheim <rsanheim@gmail.com>
2010-01-04 13:35:55 -0600

    Version 0.8.5

--- lib/chatterbox/version.rb
+++ lib/chatterbox/version.rb
@@ -3,7 +3,7 @@ module Chatterbox # :nodoc:
     unless defined?(MAJOR)
       MAJOR  = 0
       MINOR  = 8
-      TINY   = 4
+      TINY   = 5
 
       STRING = [MAJOR, MINOR, TINY].compact.join('.')
 
